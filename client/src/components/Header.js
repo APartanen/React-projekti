bf913162029cf459956f6ff6ae697242ccb7b1dd
terhi <terhi@student.uef.fi>
2021-02-07 10:13:43 +0000

    Added Header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Header.css';
+
+function Header() {
+  return (
+    <div>
+      <h1>Työharjoittelun perehdytysopas</h1>
+        <div className="Title-Subtitle">Testiteksti, jos haluaa tällaisen</div>
+    </div>
+  )
+}
+
+export default Header
