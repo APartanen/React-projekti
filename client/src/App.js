899219486ff9b9167d3a8ef2b9b7b8e66f4cc00d
terhi <terhi@student.uef.fi>
2021-02-09 21:17:52 +0000

    Lisätty Cont-Row-Col -tagit ja niiden import ulkoasua varten

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import MainNav from "./components/MainNav";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Etusivu from "./components/pages/Etusivu";
 import Lomakesivu from "./components/pages/Lomakesivu";
 import Rekrysivu from "./components/pages/Rekrysivu";
 import Footer from "./components/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
 
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
 
-
-
   return (
     <div className="App">
+
+    <Container>
+    <Row>
+    <Col>
+
       <Router>
         <Header />
         <MainNav />
@@ -24,7 +29,12 @@ function App() {
           <Route path="/lomakeinfo" component={Lomakesivu} />
         </Switch>
         <Footer />
-      </Router> 
+      </Router>
+
+    </Col>
+    </Row>
+    </Container>
+
     </div>
   );
 }
