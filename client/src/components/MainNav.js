831998eadd0afb8aaeffaa84014984c859952072
terhi <terhi@student.uef.fi>
2021-02-09 21:16:13 +0000

    Lisätty logo ja yksi classname

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Navbar, NavbarBrand, Nav, NavLink } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem } from "react-bootstrap";
+import logo from '../logo.jpg';
 
 class MainNav extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Navbar collapseOnSelect expand="lg" className="mainnav">
         <Navbar.Brand href="/">Etusivu</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -14,6 +14,9 @@ class MainNav extends React.Component {
             <Nav.Link href="lomakeinfo">
               Suunnittele rekrytointi ja jätä ilmoitus
             </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Item><img alt="" src={logo} height="80px" /></Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
