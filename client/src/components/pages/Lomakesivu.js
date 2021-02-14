459269fb20deeae1451225df860e67c020f0c601
Pietari Virtanen <pietari.virtanen@edu.karelia.fi>
2021-02-14 12:12:15 +0200

    lomake 6 ja tabit lomakesivulle

--- client/src/components/pages/Lomakesivu.js
+++ client/src/components/pages/Lomakesivu.js
@@ -2,6 +2,9 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from 'react-bootstrap/Form';
 import Lomake from '../Lomake';
+import Lomake6 from '../Lomake6';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
 // todo tyylittely
 class Lomakesivu extends React.Component {
@@ -9,8 +12,17 @@ class Lomakesivu extends React.Component {
     render() {
         return (
             <div className="align-middle">
-               <h1>Lomakesivu</h1>
-               <Lomake></Lomake>
+                <h1>Lomakesivu</h1>
+                <Tabs defaultActiveKey="placeholder" id="tabit">
+                <Tab eventKey="placeholder" title="Placeholder">
+                    <Lomake/>
+                </Tab>
+                <Tab eventKey="viimeistely" title="Viimeistely">
+                    <Lomake6/>
+                </Tab>
+
+                </Tabs>
+               
             </div>
         );
     }
