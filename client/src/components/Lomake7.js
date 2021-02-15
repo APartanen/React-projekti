89c3e32a28581949bff8c02061c611c46fcbac33
Antti Partanen <a.partanen30@gmail.com>
2021-02-15 10:02:59 +0200

    Lomake7

---
+++ client/src/components/Lomake7.js
@@ -0,0 +1,194 @@
+import { Col, Row, Form, Button } from "react-bootstrap";
+import React, { useState } from 'react';
+
+const styles = {
+  grid: {
+      paddingLeft: 5,
+      paddingRight:5
+  },
+  row: {
+      marginLeft: 15,
+      marginRight: 5
+  },
+  col: {
+      paddingLeft: 5,
+      paddingRight: 5
+  }
+};
+
+
+// alustava lomake
+const Lomake7 = (props) => {
+
+    const [submitted, setSubmitted] = useState([])
+    const [field1, setField1] = useState("")
+    const [field2, setField2] = useState("")
+    const [field3, setField3] = useState("")
+    const [field4, setField4] = useState("")
+    const [check1, setCheck1] = useState("")
+    const [check2, setCheck2] = useState("")
+    const [check3, setCheck3] = useState("")
+    const [check4, setCheck4] = useState("")
+
+    //mitä tapahtuu kun form submitataan
+    const submit = (event) => {
+    event.preventDefault()
+
+    const submitValues = [
+        {
+            kenttä1: field1,
+            kenttä2: field2,
+            kenttä3: field3,
+            kenttä4: field4,
+            box1: check1,
+            box2: check2,
+            box3: check3,
+            box4: check4
+        }
+    ]
+
+    setSubmitted(submitValues)
+    }
+
+    //tallentaa kenttään kirjoitetun arvon
+    const handleChangeField1 = (event) => {
+        event.preventDefault()
+        setField1(event.target.value)
+    }
+
+    const handleChangeField2 = (event) => {
+        event.preventDefault()
+        setField2(event.target.value)
+    }
+
+    const handleChangeField3 = (event) => {
+        event.preventDefault()
+        setField3(event.target.value)
+    }
+
+    const handleChangeField4 = (event) => {
+        event.preventDefault()
+        setField4(event.target.value)
+    }
+
+    //Checkboxit
+    const handleChangeCheck1 = (event) => {
+        const box = document.getElementById('Pitkä vaihtoehto');
+        if(box.checked == true) { 
+            setCheck1(event.target.id)
+        } else if (box.checked == false) {
+            setCheck1('')
+        }
+    }
+
+    const handleChangeCheck2 = (event) => {
+        const box = document.getElementById('Keskipitkä vaihtoehto');
+        if(box.checked == true) { 
+            setCheck2(event.target.id)
+        } else if (box.checked == false) {
+            setCheck2('')
+        }
+    }
+
+    const handleChangeCheck3 = (event) => {
+        const box = document.getElementById('Lyhyt vaihtoehto');
+        if(box.checked == true) { 
+            setCheck3(event.target.id)
+        } else if (box.checked == false) {
+            setCheck3('')
+        }
+    }
+
+    const handleChangeCheck4 = (event) => {
+        const box = document.getElementById('Ei vaihtoehtoa');
+        if(box.checked == true) { 
+            setCheck4(event.target.id)
+        } else if (box.checked == false) {
+            setCheck4('')
+        }
+    }
+
+    
+  return (
+    <div className="mx-auto my-2">
+      <h2>Lorem ipsum</h2>
+            <Form style={styles.row} onSubmit={submit}>
+
+                <Form.Group controlId="textbox1">
+
+                    <Form.Row> 
+                        <Col>
+                            <Form.Label>Lorem ipsum dolor sit?</Form.Label>
+                            <Form.Control type="text" placeholder="field1" onChange={handleChangeField1}/>
+                        </Col>
+
+                        <Col>
+                            <Form.Label>Lorem ipsum dolor sit?</Form.Label>
+                            <Form.Control type="text" placeholder="field2" onChange={handleChangeField2}/>
+                        </Col>
+                    </Form.Row>  
+
+                    <Form.Row> 
+                        <Col>
+                            <Form.Label>Lorem ipsum dolor sit?</Form.Label>
+                            <Form.Control type="text" placeholder="field3" onChange={handleChangeField3}/>
+                        </Col>
+
+                        <Col>
+                            <Form.Label>Lorem ipsum dolor sit?</Form.Label>
+                            <Form.Control type="text" placeholder="field4" onChange={handleChangeField4}/>
+                        </Col>
+                    </Form.Row>  
+                    
+                 
+                    {/**toimii spacerina */}
+                    <Form.Row> 
+                        <Col>
+                            <Form.Label></Form.Label>
+                        </Col>  
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Col>
+                            {/**Voiko valita usean?*/}
+                            <Form.Check inline label="Pitkä vaihtoehto" type="checkbox" id="Pitkä vaihtoehto" onChange={handleChangeCheck1}/>
+                            <Form.Check inline label="Keskipitkä vaihtoehto" type="checkbox" id="Keskipitkä vaihtoehto" onChange={handleChangeCheck2}/>
+                            <Form.Check inline label="Lyhyt vaihtoehto" type="checkbox" id="Lyhyt vaihtoehto" onChange={handleChangeCheck3}/>
+                            <Form.Check inline label="Ei vaihtoehtoa" type="checkbox" id="Ei vaihtoehtoa" onChange={handleChangeCheck4}/>
+                        </Col>  
+                    </Form.Row>
+                    
+                
+
+                    <Form.Row id="textarea">
+                        <Form.Label>Lorem ipsum</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Row>
+
+                        {/**toimii spacerina */}
+                    <Form.Row> 
+                        <Col>
+                            <Form.Label></Form.Label>
+                        </Col>
+                    </Form.Row>
+
+                    <Form.Row>  
+                        <Col> 
+                            <Button variant="primary" type="submit"> Submit </Button>
+                        </Col>
+    
+                        <Col>
+                            <Button variant="primary" onClick={()=>console.log(submitted)}> Tarkista arvot </Button>
+                        </Col>
+                    </Form.Row>
+
+                    
+
+                </Form.Group>
+
+            </Form>
+    </div>
+  )
+}
+
+export default Lomake7;
--- client/src/components/pages/Lomakesivu.js
+++ client/src/components/pages/Lomakesivu.js
@@ -5,7 +5,7 @@ import Lomake from '../Lomake';
 import Lomake6 from '../Lomake6';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-
+import Lomake7 from '../Lomake7'
 // todo tyylittely
 class Lomakesivu extends React.Component {
 
@@ -21,6 +21,10 @@ class Lomakesivu extends React.Component {
                     <Lomake6/>
                 </Tab>
 
+                <Tab eventKey="Lomake7" title="Lomake7">
+                    <Lomake7/>
+                </Tab>
+                
                 </Tabs>
                
             </div>
