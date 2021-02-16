1572bc328ee7c7d69d5e6767c9cd75bf3f9a5353
atte.katainen <atte.katainen@gmail.com>
2021-02-16 13:49:49 +0200

    Lisätty lomakkeeseen vaihe 2

---
+++ client/src/components/Lomake2.js
@@ -0,0 +1,153 @@
+import React, { Component, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Link } from "react-router-dom";
+import "./Lomake2.css";
+import { Form, Dropdown } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function Lomake2() {
+  const [selectedDate1, setSelectedDate1] = useState(null);
+  const [selectedDate2, setSelectedDate2] = useState(null);
+  return (
+    <div>
+      <div className="screen">
+        <div className="otsikko">
+          <h1>Työaika ja työnkesto</h1>
+          <h6>Arvioi, kuinka paljon työtä sinulla on tarjota työntekijälle.</h6>
+        </div>
+
+        <label>
+          Työsopimuksen tyyppi *
+          <small id="emailHelp" class="form-text text-muted">
+            Tämä tieto näkyy ilmoituksessa.
+          </small>
+        </label>
+
+        <div className="lomake">
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            ></input>
+            <label class="form-check-label" for="flexCheckDefault">
+              Määräaikainen
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            ></input>
+            <label class="form-check-label" for="flexCheckDefault">
+              Toistaiseksi voimassa oleva
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            ></input>
+            <label class="form-check-label" for="flexCheckDefault">
+              Tarvittaessa töihin kutsuttava
+            </label>
+          </div>
+        </div>
+
+        <label>
+          Työsopimuksen kesto *
+          <small class="form-text text-muted">
+            Tämä tieto näkyy ilmoituksessa.
+          </small>
+        </label>
+
+        <div className="date">
+          <div>
+            <label>Työ alkaa</label>
+            <br></br>
+            <DatePicker
+              id="juu"
+              selected={selectedDate1}
+              onChange={(date) => setSelectedDate1(date)}
+              placeholderText={"dd/mm/yyyy"}
+              // minDate={new Date()}
+              // maxDate={new Date()}
+              filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+              showYearDropdown
+              scrollableYearDropdown
+            />
+          </div>
+          <div>
+            <label>Työ päättyy</label>
+            <br></br>
+            <DatePicker
+              selected={selectedDate2}
+              onChange={(date) => setSelectedDate2(date)}
+              placeholderText={"dd/mm/yyyy"}
+              // minDate={new Date()}
+              // maxDate={new Date()}
+              filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+              showYearDropdown
+              scrollableYearDropdown
+            />
+          </div>
+        </div>
+        <form className="tunnit1">
+          <div class="form-group" className="tunnit2">
+            <label>
+              Tuntien määrä / viikko
+              <small class="form-text text-muted">
+                Tämä tieto näkyy ilmoituksessa.
+              </small>
+            </label>
+
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder=""
+            ></input>
+            <small class="form-text text-muted"></small>
+          </div>
+        </form>
+        <Dropdown>
+          <label>
+            Työaika *
+            <small class="form-text text-muted">
+              Tämä tieto näkyy ilmoituksessa.
+            </small>
+          </label>
+          <br></br>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Valitse
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Osa-aikainen</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Kokoaikainen</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <div className="links">
+        <Link to="Lomakeinfo">
+          <button type="button" class="btn btn-outline-success">
+            Edellinen
+          </button>
+        </Link>
+        <button type="button" class="btn btn-outline-success">
+          Seuraava
+        </button>
+      </div>
+    </div>
+  );
+}
--- client/src/components/pages/Lomakesivu.js
+++ client/src/components/pages/Lomakesivu.js
@@ -1,39 +1,41 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Form from 'react-bootstrap/Form';
-import Lomake from '../Lomake';
-import Lomake5 from '../Lomake5';
-import Lomake6 from '../Lomake6';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
-import Lomake7 from '../Lomake7'
+import Form from "react-bootstrap/Form";
+import Lomake from "../Lomake";
+import Lomake5 from "../Lomake5";
+import Lomake6 from "../Lomake6";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import Lomake7 from "../Lomake7";
+import Lomake2 from "../Lomake2";
 // todo tyylittely
 class Lomakesivu extends React.Component {
+  render() {
+    return (
+      <div className="align-middle">
+        <h1>Lomakesivu</h1>
+        <Tabs defaultActiveKey="Perustiedot" id="tabit">
+          <Tab eventKey="Perustiedot" title="Perustiedot">
+            <Lomake />
+          </Tab>
+          <Tab eventKey="valintamenettely" title="Käytettävä valintamenettely">
+            <Lomake5 />
+          </Tab>
+          <Tab eventKey="viimeistely" title="Viimeistely">
+            <Lomake6 />
+          </Tab>
 
-    render() {
-        return (
-            <div className="align-middle">
-                <h1>Lomakesivu</h1>
-                <Tabs defaultActiveKey="placeholder" id="tabit">
-                <Tab eventKey="perustiedot" title="Perustiedot">
-                    <Lomake/>
-                </Tab>
-                <Tab eventKey="valintamenettely" title="Käytettävä valintamenettely">
-                    <Lomake5/>
-                </Tab>
-                <Tab eventKey="viimeistely" title="Viimeistely">
-                    <Lomake6/>
-                </Tab>
+          <Tab eventKey="Lomake7" title="Lomake7">
+            <Lomake7 />
+          </Tab>
 
-                <Tab eventKey="Lomake7" title="Lomake7">
-                    <Lomake7/>
-                </Tab>
-                
-                </Tabs>
-               
-            </div>
-        );
-    }
+          <Tab eventKey="Lomake2" title="Työaika ja työnkesto">
+            <Lomake2 />
+          </Tab>
+        </Tabs>
+      </div>
+    );
+  }
 }
 
 export default Lomakesivu;
