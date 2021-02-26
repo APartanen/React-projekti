f7ffb78ed5f39466f925106c422e3382df677063
terhi <terhi@student.uef.fi>
2021-02-26 16:22:01 +0000

    Updated Lomake.js

--- client/src/components/Lomake.js
+++ client/src/components/Lomake.js
@@ -1,92 +1,130 @@
-import React from "react";
-import { Col, Row, Form, Button, Container } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Col, Row, Form, Button, Container, Modal } from "react-bootstrap";
+import * as ReactBootstrap from "react-bootstrap";
 
-const styles = {
-  grid: {
-    paddingLeft: 5,
-    paddingRight: 5,
-  },
-  row: {
-    marginLeft: 15,
-    marginRight: 5,
-  },
-  col: {
-    paddingLeft: 5,
-    paddingRight: 5,
-  },
-};
 
 // alustava lomake
 function Lomake() {
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="mx-auto my-2">
-      <h2>Perustiedot yrityksestä</h2>
-      <Form style={styles.row}>
-        <Col xs="5">
-          <Form.Group id="YrityksenNimi">
-            <Form.Label>Yrityksen nimi</Form.Label>
-            <Form.Control type="yrityksennimi" placeholder="Yrityksen nimi" />
-            <Form.Text className="text-muted">
-              Tämä tieto näkyy ilmoituksessa.
-            </Form.Text>
-          </Form.Group>
-        </Col>
 
-        <Col xs="5">
-          <Form.Group id="YrityksenOsoite">
-            <Form.Label>Katuosoite</Form.Label>
-            <Form.Control placeholder="Missä yrityksesi sijaitsee?" />
-            <Form.Text className="text-muted">
-              Tämä tieto näkyy ilmoituksessa.
-            </Form.Text>
-          </Form.Group>
-        </Col>
-
-        <Form.Row>
-          <Col xs="auto">
-            <Form.Group id="Postitoimipaikka">
-              <Form.Label>Postitoimipaikka</Form.Label>
-              <Form.Control />
-              <Form.Text className="text-muted">
-                Tämä tieto näkyy ilmoituksessa.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group id="Postinumero">
-              <Form.Label>Postinumero</Form.Label>
-              <Form.Control />
-              <Form.Text className="text-muted">
-                Tämä tieto näkyy ilmoituksessa.
-              </Form.Text>
-            </Form.Group>
-          </Col>
-
-          <Col xs="4">
-            <Form.Row id="Kuvaus">
-              <Form.Label>Yrityksen kuvaus</Form.Label>
-              <Form.Control as="textarea" rows={6} />
-              <Form.Text className="text-muted">
-                Tämä tieto näkyy ilmoituksessa.
-              </Form.Text>
-            </Form.Row>
-          </Col>
-        </Form.Row>
-
-        <Form.Row>
-          <Col xs="4">
-            <Form.Label>Yrityksen Logo</Form.Label>
-            <Form.File id="logo" label="Valitse yrityksen logo..." custom />
-          </Col>
-        </Form.Row>
-
-        <Form.Group>
-          <Button variant="primary" size="lg" active>
-            ETEENPÄIN!
-          </Button>{" "}
-        </Form.Group>
-      </Form>
+    <div className="otsikko">
+      <h2>Perustiedot</h2>
+      <h6>Yrityksen ja työpaikan yhteystiedot.</h6>
     </div>
-  );
+
+    <hr />
+
+      <Form>
+
+        <Row>
+          <Col>
+            <Form.Label>Perehdytettävä</Form.Label>
+            <Form.Control type="text" placeholder="Perehdytettävän nimi" />
+          </Col>
+          <Col>
+            <Form.Label>Perehdyttäjä</Form.Label>
+            <Form.Control type="text" placeholder="Perehdyttäjän nimi" />
+          </Col>
+        </Row>
+
+        <Row>
+            <Col>
+            <Form.Label>Perehdytys alkaa</Form.Label>
+            <Form.Control type="date" name="pvm" />
+            </Col>
+            <Col>
+            <Form.Label>Perehdytys päättyy</Form.Label>
+            <Form.Control type="date" name="pvm" />
+          </Col>
+        </Row>
+
+    <hr />
+
+        <Row>
+          <Col>
+            <Form.Label>Yrityksen nimi, osoite, puhelinnumero, faksi, sähköposti, verkkosivut</Form.Label>
+          </Col>
+          <Col>
+          <Form.Label>Työpaikan nimi, osoite, puhelinnumero, sähköposti</Form.Label>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+          <Form.Group>
+            <Form.Control type="text" placeholder="Yrityksen nimi" />
+            <Form.Control type="text" placeholder="Yrityksen katuosoite" />
+            <Form.Control type="text" placeholder="Yrityksen puhelinnumero" />
+            <Form.Control type="text" placeholder="Yrityksen faksi" />
+            <Form.Control type="text" placeholder="Yrityksen sähköposti" />
+            <Form.Control type="text" placeholder="Yrityksen verkkosivut" />
+          </Form.Group>
+          </Col>
+
+          <Col>
+            <Form.Control type="text" placeholder="Työpaikan nimi" />
+            <Form.Control type="text" placeholder="Työpaikan katuosoite" />
+            <Form.Control type="text" placeholder="Työpaikan puhelinnumero" />
+            <Form.Control type="text" placeholder="Työpaikan sähköposti" />
+          </Col>
+
+        </Row>
+
+        <Row>
+          <Col>
+          <Form.Label>Yrityksen Logo</Form.Label>
+          <Form.File id="logo" label="Valitse yrityksen logo..." custom />
+
+
+
+          <Form.Label>Yrityksen kuvaus</Form.Label>
+          <Button variant="primary" onClick={handleShow}>Vinkki</Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Yrityksen kuvaus</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>
+              Mitä toivot uuden ihmisen tietävän yhtiöstä ja mitä ehdottomasti pitää sisäistää?
+              </p>
+              <p>
+              Esimerkiksi:<br />
+              – yrityksen toiminta-ajatus, liike- ja palveluidea<br />
+              – yrityksen omistussuhteet<br />
+              – asiakkaat ja heidän odotuksensa<br />
+              – lisätietoa yrityksestä (esitteet, Internet, intranet jne.)<br />
+              – kilpailijat
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={handleClose}>
+                Sulje
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
+          <Form.Control as="textarea" rows={6} />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="success">Edellinen</Button>{' '}
+            <Button variant="success">Seuraava</Button>
+          </Col>
+        </Row>
+
+      </Form>
+
+    </div>
+  )
 }
 
 export default Lomake;
