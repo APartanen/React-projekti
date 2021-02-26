d8a819d834a3797b4dbe50c5097ac758a4645325
terhi <terhi@student.uef.fi>
2021-02-26 16:21:46 +0000

    Updated Lomakesivu.js

--- client/src/components/pages/Lomakesivu.js
+++ client/src/components/pages/Lomakesivu.js
@@ -1,51 +1,69 @@
 import React from "react";
-import { useState } from "react";
+
 import Lomake from "../Lomake";
+import Lomake1 from "../Lomake1";
 import Lomake2 from "../Lomake2";
+import Lomake3 from "../Lomake3";
 import Lomake4 from "../Lomake4";
 import Lomake5 from "../Lomake5";
 import Lomake6 from "../Lomake6";
 import Lomake7 from "../Lomake7";
-import Lomake8 from "../Lomake8";
+import Lomake9 from "../Lomake9";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 
+
 // todo tyylittely
-const Lomakesivu = () => {
-  const [tiedot, setTiedot] = useState([]);
+class Lomakesivu extends React.Component {
+  render() {
+    return (
+      <div className="align-middle">
 
-  return (
-    <div className="align-middle">
-      <h1>Lomakesivu</h1>
-      <Tabs defaultActiveKey="Perustiedot" id="tabit">
-        <Tab eventKey="Perustiedot" title="Perustiedot">
-          <Lomake />
-        </Tab>
-        <Tab eventKey="Työaika ja työnkesto" title="Työaika ja työnkesto">
-          <Lomake2 />
-        </Tab>
+        <h1>Uusi perehdytysopas</h1>
+        <p>
+        Tämän lomakkeen avulla voit laatia harjoittelijallesi tulostettavan perehdytysoppaan.
+        </p>
+        <p>
+        Perehdyttämisen tarkistuslista sisältää jäsennellyn luettelon perehdyttämisessä esille otettavia asioita suunnittelun ja toteutuksen tueksi. Perehdyttämisen seurannassa ja oppimisen varmistamisessa listaa käyttävät sekä perehdyttäjä että perehdytettävä. Listaa saa työpaikalla kopioida, karsia ja täydentää.
+        </p>
 
-        <Tab eventKey="Tehtävään haku" title="Tehtävään haku">
-          <Lomake4 />
-        </Tab>
-        <Tab eventKey="valintamenettely" title="Käytettävä valintamenettely">
-          <Lomake5 />
-        </Tab>
-        <Tab eventKey="viimeistely" title="Viimeistely">
-          <Lomake6 />
-        </Tab>
-        <Tab eventKey="Testisivu" title="Testisivu">
-          <Lomake7 call={setTiedot} array={tiedot} />
-        </Tab>
+        <div className="opasWrapper">
+        <Tabs defaultActiveKey="Perustiedot" id="tabit">
+          <Tab eventKey="Perustiedot" title="Perustiedot">
+            <Lomake />
+          </Tab>
+          <Tab eventKey="Yrityksen organisaatio ja toimintatavat" title="Yrityksen organisaatio ja toimintatavat">
+            <Lomake1 />
+          </Tab>
+          <Tab eventKey="Työsopimus" title="Työsopimus">
+            <Lomake2 />
+          </Tab>
+          <Tab eventKey="Työympäristö" title="Työympäristö">
+            <Lomake9 />
+          </Tab>
+          <Tab eventKey="Työtehtävät" title="Työtehtävät">
+            <Lomake3 />
+          </Tab>
+          <Tab eventKey="Tehtävään haku" title="Koulutus ja sisäinen tiedottaminen">
+            <Lomake4 />
+          </Tab>
+          <Tab eventKey="Muuta" title="Muuta">
+            <Lomake5 />
+          </Tab>
+          <Tab eventKey="viimeistely" title="Viimeistely">
+            <Lomake6 />
+          </Tab>
+          <Tab eventKey="Testisivu" title="Testisivu">
+            <Lomake7 />
+          </Tab>
+        </Tabs>
+        </div>
 
-        <Tab eventKey="Tietojen kooste" title="Tietojen kooste">
-          <Lomake8 array={tiedot} />
-        </Tab>
-      </Tabs>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default Lomakesivu;
